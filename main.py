a923311b06789ae47f1b16b829590a43ce1f6701
kenneth <103740929+Johnkennethdecastro@users.noreply.github.com>
2022-04-29 00:07:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import random
 pygame.font.init()
 pygame.mixer.init()
+
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Covid Impact")
