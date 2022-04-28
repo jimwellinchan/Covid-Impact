70d357c894fc7bb2c4f19b7fa7d9288291831e81
aldrincustodio <103046113+aldrincustodio@users.noreply.github.com>
2022-04-28 23:11:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,6 @@ shoot_fx.set_volume(0.5)
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "infected red.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "infected green.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "infected blue.png"))
+
+# Player player
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "nurse.png"))
