314a1e8863aa7aa387ecba11db165f81d90fc884
kenneth <103740929+Johnkennethdecastro@users.noreply.github.com>
2022-04-28 23:35:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -92,3 +92,11 @@ class Ship:
 
     def get_height(self):
         return self.ship_img.get_height()
+
+class Player(Ship):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = YELLOW_SPACE_SHIP
+        self.laser_img = YELLOW_LASER
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
