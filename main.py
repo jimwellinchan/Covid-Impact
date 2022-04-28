b07f22527c11f1dbb9c01a589b16f4978e894fa1
kenneth <103740929+Johnkennethdecastro@users.noreply.github.com>
2022-04-28 23:41:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -128,3 +128,7 @@ class Enemy(Ship):
                 "green": (GREEN_SPACE_SHIP, GREEN_LASER),
                 "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
                 }
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
