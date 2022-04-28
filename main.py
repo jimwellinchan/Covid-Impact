8baf44596c15c803c4ab9140aa0784cdb5f02dbf
kenneth <103740929+Johnkennethdecastro@users.noreply.github.com>
2022-04-28 23:19:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,22 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "syringe.png"))
 
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "Lab Background.png")), (WIDTH, HEIGHT))
+
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return not(self.y <= height and self.y >= 0)
+
+    def collision(self, obj):
+        return collide(self, obj)
