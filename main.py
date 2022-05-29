def8d2830930a2d5a2dc0ac57967bb1e7ed99b7b
Johnkennethdecastro <103740929+Johnkennethdecastro@users.noreply.github.com>
2022-05-30 01:07:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import os
 from pygame import mixer
 import time
 import random
+
 pygame.font.init()
 pygame.mixer.init()
-
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Covid Impact")
 
 shoot_fx = pygame.mixer.Sound('assets/Syringe.mp3')
 shoot_fx.set_volume(0.5)
+
+GameOver_fx = pygame.mixer.Sound('assets/GameOver.mp3')
+GameOver_fx.set_volume(0.5)
+
+Damaged_fx = pygame.mixer.Sound('assets/Damaged.mp3')
+Damaged_fx.set_volume(0.5)
 
 # Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "infected red.png"))
